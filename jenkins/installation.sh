b3e26c3de33d82a3b8384f3fae8dbc2726949865
root <root@ip-10-0-0-167.ec2.internal>
2025-01-27 16:11:29 +0000

    added the installation code for Jenkins

---
+++ jenkins/installation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+wget -O /etc/yum.repos.d/jenkins.repo \
+	    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+yum upgrade #which updates all the dependencies
+yum install java-17 -y #insatll java version 17
+yum install jenkins -y #install jenkins package 
+systemctl daemon-reload
+systemctl enable jenkins #which automatically start jenkins when the reboot happens
+systemctl start jenkins #start the jenkins 
+systemctl status jenkins #check status of jenkins
