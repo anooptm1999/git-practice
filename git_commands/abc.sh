fad3ad00ee2fc2cc06f4cef78eaf375b66607c6a
root <root@ip-10-0-0-247.ec2.internal>
2025-01-27 14:42:11 +0000

    added a file

---
+++ git_commands/abc.sh
@@ -0,0 +1,18 @@
+yum install git -y #install git to linux machine
+git add perticular file name #update that perticular data to repo
+git add ./ git add * #update all the contents in git
+git push #which add all the updated data to the remote repo
+git commit -m "..." #helps in tracking 
+git remote -v #which tells wheather our working repo is connected to any git account or not 
+git log -p #which gives the o/p of all the tracking and the updates wrt time & git id aswell
+git clone #helps in cloning the public directory on generating ssh_key
+git config --global user.name/user.email #helps in updating our git info
+git pull #helps in retriving the data from remote server to local machine
+git branch #executes how many branches arde there
+git branch -a #shows all remote branches
+git branch specified name #create a branch with that name
+git init #initialize the .git file
+git switch/checkout specified branch name #switches the branch with specified name given
+git branch -d specified name #deletes that specified branch which is specified in command
+git merge abc #which merges this abc branch with main branch
+git reset specified filename #which undo the changes
