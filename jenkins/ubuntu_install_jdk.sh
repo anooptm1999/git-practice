a8119e050302495e074af9ed4c5e7eaf615ec0f2
Anoop T M <130398830+anooptm1999@users.noreply.github.com>
2025-05-01 00:28:33 +0530

    Update ubuntu_install_jdk.sh

--- jenkins/ubuntu_install_jdk.sh
+++ jenkins/ubuntu_install_jdk.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# Update package list
 sudo apt update -y
-
-# Install Java (Jenkins requires Java 11 or newer; OpenJDK 17 is recommended)
-sudo apt install -y openjdk-17-jdk
-
-# Verify Java installation
+apt install -y openjdk-17-jd
 java -version
