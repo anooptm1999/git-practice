4a8e3013d196851d87efdea1e696897577362f74
Anoop T M <130398830+anooptm1999@users.noreply.github.com>
2025-05-08 23:52:36 +0530

    Create plan-info.sh

---
+++ git_commands/plan-info.sh
@@ -0,0 +1,5 @@
+#step 1: create a cloned ec2 with git 
+#step 2: enable the token generated from git settings>>devoloper settings>>classic tokens
+#step 3: use export user="" && token="" & install jk
+#step 4: run the script info.sh by giving chmod permission like ./info.sh gitrepo name with branch 
+#step 5: finally gives the o/p with all the names
