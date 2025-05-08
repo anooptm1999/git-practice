ca56f226f50f3d82a33e4c0277de2c74a1ac939d
Anoop T M <130398830+anooptm1999@users.noreply.github.com>
2025-05-09 01:20:07 +0530

    Update MyWebAppApplication.java

---
+++ project/src/main/java/com/example/MyWebAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.mywebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class MyWebAppApplication extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(MyWebAppApplication.class, args);
+    }
+}
