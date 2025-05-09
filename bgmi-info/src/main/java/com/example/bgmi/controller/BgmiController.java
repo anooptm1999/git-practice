2b70b07cd8b5a8c470e8c4ebdbaf7d8c97848098
Anoop T M <130398830+anooptm1999@users.noreply.github.com>
2025-05-09 23:52:51 +0530

    Update BgmiController.java

--- bgmi-info/src/main/java/com/example/bgmi/controller/BgmiController.java
+++ bgmi-info/src/main/java/com/example/bgmi/controller/BgmiController.java
@@ -1,13 +1,22 @@
 package com.example.bgmi.controller;
 
+import com.example.bgmi.service.BgmiService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BgmiController {
 
+    private final BgmiService bgmiService;
+
+    @Autowired
+    public BgmiController(BgmiService bgmiService) {
+        this.bgmiService = bgmiService;
+    }
+
     @GetMapping("/status")
     public String status() {
-        return "BGMI Info Service is running!";
+        return bgmiService.getStatusMessage(); // now using the service layer
     }
 }
