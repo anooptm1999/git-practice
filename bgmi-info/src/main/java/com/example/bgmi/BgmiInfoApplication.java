9dfcd8786417595a8071b8c98a92e4da41a2e0c8
Anoop T M <130398830+anooptm1999@users.noreply.github.com>
2025-05-10 01:40:18 +0530

    Update BgmiInfoApplication.java

--- bgmi-info/src/main/java/com/example/bgmi/BgmiInfoApplication.java
+++ bgmi-info/src/main/java/com/example/bgmi/BgmiInfoApplication.java
@@ -1,3 +1,10 @@
+package com.example.bgmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 @SpringBootApplication
 public class BgmiInfoApplication extends SpringBootServletInitializer {
 
