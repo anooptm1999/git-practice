c3964ecfb359a751de246e6b4b482b1462b33d6b
Anoop T M <130398830+anooptm1999@users.noreply.github.com>
2025-05-09 23:19:12 +0530

    Update BgmiInfoApplication.java

---
+++ bgmi-info/src/main/java/com/example/bgmi/BgmiInfoApplication.java
@@ -0,0 +1,11 @@
+package com.example.bgmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgmiInfoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BgmiInfoApplication.class, args);
+    }
+}
