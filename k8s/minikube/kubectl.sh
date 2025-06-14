caa0158ea05ecff1594a44f86da364f6955005ef
Anoop T M <130398830+anooptm1999@users.noreply.github.com>
2025-06-14 19:10:22 +0530

    Update kubectl.sh

--- k8s/minikube/kubectl.sh
+++ k8s/minikube/kubectl.sh
@@ -1,1 +1,13 @@
+# Install kubectl
+#!/bin/bash
+set -e
 
+# Use -L to follow redirects!
+KUBECTL_VERSION=$(curl -sL https://dl.k8s.io/release/stable.txt)
+echo "Latest kubectl version: $KUBECTL_VERSION"
+
+curl -LO "https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+
+kubectl version --client
