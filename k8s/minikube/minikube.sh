4dd891fdfbaaffd992a921ff4139e045a532f953
root <root@ip-192-0-0-174.ap-south-1.compute.internal>
2025-06-14 14:12:11 +0000

    changed minikube

--- k8s/minikube/minikube.sh
+++ k8s/minikube/minikube.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -e
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-chmod +x minikube
-minikube start --driver=docker
-minikube status
+
