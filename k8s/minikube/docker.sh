b602144c9e5f2c67ac5ac6789693dc66ba16f1e3
Anoop T M <130398830+anooptm1999@users.noreply.github.com>
2025-06-14 12:55:30 +0530

    Delete k8s/minikube/docker.sh

--- k8s/minikube/docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Install Docker
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io
-
-# Enable Docker without sudo
-sudo usermod -aG docker ubuntu
-newgrp docker
