7d4f5e87865ed81888f8f3d666a0bc20c813b40f
Anoop T M <130398830+anooptm1999@users.noreply.github.com>
2025-06-14 12:50:11 +0530

    Update docker.sh

--- k8s/minikube/docker.sh
+++ k8s/minikube/docker.sh
@@ -1,16 +1,8 @@
-# Add Docker’s official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-# Add Docker repository
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] \
-  https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
-  | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
+#!/bin/bash
 # Install Docker
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io
 
 # Enable Docker without sudo
-sudo usermod -aG docker $USER
+sudo usermod -aG docker ubuntu
 newgrp docker
