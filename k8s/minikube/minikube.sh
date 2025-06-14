749bddc2210d5c04c53e2a68f3dc4dd764d3f075
root <root@ip-192-0-0-174.ap-south-1.compute.internal>
2025-06-14 14:21:58 +0000

    a

--- k8s/minikube/minikube.sh
+++ k8s/minikube/minikube.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-install minikube-linux-amd64 /usr/local/bin/minikube
-rm minikube-linux-amd64
+curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
 
