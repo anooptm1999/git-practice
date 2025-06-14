438a61029b2255713827e8407b325d2efb764c7f
Anoop T M <130398830+anooptm1999@users.noreply.github.com>
2025-06-14 19:09:30 +0530

    Update dependencies.sh

--- k8s/minikube/dependencies.sh
+++ k8s/minikube/dependencies.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
 
+# Update system
+sudo apt-get update -y && sudo apt-get upgrade -y
+
+# Install required packages
+sudo apt-get install -y curl wget apt-transport-https ca-certificates gnupg lsb-release conntrack
