409ffe1d7632b95f233c4d632fd5ae3088094216
root <root@ip-192-0-0-174.ap-south-1.compute.internal>
2025-06-14 14:10:38 +0000

    costumized the minikube

--- k8s/minikube/minikube.sh
+++ k8s/minikube/minikube.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
-# Install Minikube
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube-linux-amd64 /usr/local/bin/minikube
+set -e
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+chmod +x minikube
+minikube start --driver=docker
+minikube status
