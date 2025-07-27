9212581275d54869809862d45fe4a30521f21e5e
Anoop T M <130398830+anooptm1999@users.noreply.github.com>
2025-07-27 14:08:54 +0530

    Update loops.sh

--- shell/loops.sh
+++ shell/loops.sh
@@ -11,3 +11,33 @@ while [ "$count" -le 5 ]; do
     echo "Count is $count"
     ((count++))
 done
+
+####################################################
+
+#!/bin/bash
+
+read -p "the class obtained by the student by percentile method " percentage
+
+echo "entered percentage is: $percentage"
+
+if [[ $percentage -ge 35 && $percentage -lt 50 ]]; then
+        echo "pass"
+elif [[ $percentage -ge 50 && $percentage -lt 60 ]]; then
+        echo "second class"
+elif [[ $percentage -ge 60 && $percentage -lt 84.999 ]]; then
+        echo "first class"
+else
+        echo "FCD"
+fi
+
+###########################################################
+
+#!/bin/bash
+echo "even numbers from 50 to 100 are: "
+for i in {50..100}; do
+if (( i%2 == 0 )); then
+        echo $i
+else
+        continue
+fi
+done
