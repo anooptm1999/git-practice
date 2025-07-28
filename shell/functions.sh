28246f5a9fdc78a8d7381c0911a1eca2d93da130
Anoop T M <130398830+anooptm1999@users.noreply.github.com>
2025-07-28 12:45:01 +0530

    Update functions.sh

--- shell/functions.sh
+++ shell/functions.sh
@@ -25,3 +25,40 @@ if (( num % 2 == 0 )); then
 fi }
 
 is_even
+
+################################################################
+
+#!/bin/bash
+
+##################################
+# author: ATM
+# date: 28-07-2025
+#################################
+
+set -x
+set -e
+set -o pipefail
+
+destination_path="/opt/syslog"
+time=$(date +%F_%H-%M-%S)
+destination_file_name="$destination_path/reports_$time.log"
+
+sudo mkdir -p "$destination_path"
+sudo chown "$USER":"$USER" "$destination_path"
+
+performance () {
+free -h
+nproc
+du -h
+ps -eaf | head
+
+} > "$destination_file_name"
+
+echo "********************* exection started ************"
+
+if ! performance; then
+       echo "fail to execute"
+        exit 1
+fi
+
+echo "********************** execution done *************"
