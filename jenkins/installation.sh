fc4e88236c5625fecbcf2cf8ddccde3e7956aebb
Anoop T M <anooptm1999@gmail.com>
2025-12-30 15:49:36 +0530

    Update installation.sh

--- jenkins/installation.sh
+++ jenkins/installation.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run this script as root or using sudo"
+  exit 1
+fi
+
 wget -O /etc/yum.repos.d/jenkins.repo \
 https://pkg.jenkins.io/redhat-stable/jenkins.repo
 
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 
 yum upgrade -y
-
 yum install java-17-openjdk -y
 yum install jenkins -y
 
