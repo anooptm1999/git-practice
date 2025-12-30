8da03f22a49b60eae57cecae4d64a837ce9dabae
Anoop T M <anooptm1999@gmail.com>
2025-12-30 15:50:40 +0530

    Update installation.sh

--- jenkins/installation.sh
+++ jenkins/installation.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
-
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run this script as root or using sudo"
-  exit 1
-fi
-
+#installing in linux machine
 wget -O /etc/yum.repos.d/jenkins.repo \
-https://pkg.jenkins.io/redhat-stable/jenkins.repo
-
+	    https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-
-yum upgrade -y
-yum install java-17-openjdk -y
-yum install jenkins -y
-
+yum upgrade #which updates all the dependencies
+yum install java-17 -y #insatll java version 17
+yum install jenkins -y #install jenkins package 
 systemctl daemon-reload
-systemctl enable jenkins
-systemctl start jenkins
-systemctl status jenkins
+systemctl enable jenkins #which automatically start jenkins when the reboot happens
+systemctl start jenkins #start the jenkins 
+systemctl status jenkins #check status of jenkins why this is not working
