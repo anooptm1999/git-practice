df27a709d7e75b251c3943d96c15f75a04fb121a
Anoop T M <anooptm1999@gmail.com>
2025-12-30 15:48:06 +0530

    Update installation.sh

--- jenkins/installation.sh
+++ jenkins/installation.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-#installing in linux machine
+
 wget -O /etc/yum.repos.d/jenkins.repo \
-	    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-yum upgrade #which updates all the dependencies
-yum install java-17 -y #insatll java version 17
-yum install jenkins -y #install jenkins package 
+
+yum upgrade -y
+
+yum install java-17-openjdk -y
+yum install jenkins -y
+
 systemctl daemon-reload
-systemctl enable jenkins #which automatically start jenkins when the reboot happens
-systemctl start jenkins #start the jenkins 
-systemctl status jenkins #check status of jenkins
+systemctl enable jenkins
+systemctl start jenkins
+systemctl status jenkins
